a6c10f925b4c355064903c172da4e31b14dd67cf
shenyintao <46699220+shenyintao@users.noreply.github.com>
2019-01-19 16:13:21 +0800

    更换了重置文件位置

---
+++ src/components/shenyintao/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <Search></Search>
+  </div>
+</template>
+
+<script>
+import Search from '@/components/shenyintao/Search'
+export default {
+  name: 'Home',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/components/shenyintao/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="search">
+    <div class="logo">
+      <!--<img src="../../assets/img/homelogo.png" />-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped lang="less">
+  .logo{
+    width: 100px;
+    height: 100px;
+    background: #000;
+  }
+</style>
