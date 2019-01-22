3e32d7f13160105edea2e66ce945783a5ac3b1b7
chuangwang142 <1423958370@qq.com>
2019-01-22 16:01:19 +0800

    加了一个页面，配置了登录页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/shenyintao/Home'
-
+import Login from '@/components/wangchaung/Login'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
