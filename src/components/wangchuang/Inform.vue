e79358038af5f3ebccbfa86e1056e6b58a42beef
chuangwang142 <1423958370@qq.com>
2019-02-15 14:54:12 +0800

    修改了路径报错问题

--- src/components/wangchuang/Inform.vue
+++ src/components/wangchuang/Inform.vue
@@ -99,9 +99,9 @@
 </template>
 
 <script>
-import Headers from "../common/Header";
-import Foot from "../common/Footer";
-import Search from "../shenyintao/Search";
+import Headers from '@/components/common/Header'
+import Foot from '@/components/common/Footer'
+import Search from '@/components/shenyintao/Search'
 export default {
   name: 'Inform',
   data () {
